93ed69f2b946e8aa1a70b882af0a52066c6e067b
Arief Darmawan <121046920+ariefdrm@users.noreply.github.com>
2025-06-29 08:26:18 +0700

    "updates: remove dist folder"

--- dist/utils/getDataFromDb.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getEmailFromDb = getEmailFromDb;
-const db_1 = require("../configs/db");
-async function getEmailFromDb(email) {
-    const query = "SELECT email FROM users WHERE email = $1;";
-    const result = await db_1.client.query(query, [email]);
-    return result.rows;
-}
